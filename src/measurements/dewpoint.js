5b14aff5192f47df2317fc080f4ec6ce32f80736
Julia <uug11@mail.ru>
2025-08-07 16:31:09 +0400

    added info for dew point

--- src/measurements/dewpoint.js
+++ src/measurements/dewpoint.js
@@ -7,5 +7,57 @@ export default {
   nameshort: {
     en: "Dew point",
     ru: "Точка росы"
-  }
-};
+  },
+  unit: "℃",
+  chartColor: "#6fd3ff",
+  range: [0, 15, 18, 21, 24],
+  zones: [
+    {
+      value: 10,
+      color: "#ff4d00",
+      label: {
+        en: "Very Dry Air",
+        ru: "Очень сухой воздух"
+      }
+    },
+    {
+      value: 15,
+      color: "#ff9d00",
+      label: {
+        en: "Dry",
+        ru: "Сухо"
+      }
+    },
+    {
+      value: 18,
+      color: "#60bc2a",
+      label: {
+        en: "Comfortable",
+        ru: "Комфортно"
+      }
+    },
+    {
+      value: 21,
+      color: "#12bfcc",
+      label: {
+        en: "Humid",
+        ru: "Влажно"
+      }
+    },
+    {
+      value: 24,
+      color: "#2a5cbc",
+      label: {
+        en: "High Humidity Level",
+        ru: "Высокий уровень влажности"
+      }
+    },
+    {
+      color: "#7a00da",
+      label: {
+        en: "Extremely Humid",
+        ru: "Очень влажно"
+      }
+    }
+  ],
+}
